7979cbbc26ebf9fcbb3c35c17ed8cffb19ac55e6
seyvb <seyvb@seykol.com>
2020-07-19 15:04:59 +0300

    Libary projesi eklendi

---
+++ SeyKol.Library/Interface/IComputerTrace.cs
@@ -0,0 +1,17 @@
+﻿namespace SeyKol.StudyAnalysis.Library.Interface
+{
+    interface IComputerTrace
+    {
+        /// <summary>
+        /// Bilgisayarın boşta kalma zamanını milisaniye cinsinden döndürür
+        /// </summary>
+        /// <returns></returns>
+        uint GetIdleTime();
+
+        /// <summary>
+        /// Bilgisayarın son kullanıldığı zamanı döndürür
+        /// </summary>
+        /// <returns></returns>
+        uint GetLastInputTime();
+    }
+}
---
+++ SeyKol.Library/Process/ComputerTraceProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeyKol.StudyAnalysis.Library.Interface;
+using SeyKol.StudyAnalysis.Library.ProcessHelper;
+
+namespace SeyKol.StudyAnalysis.Library.Process
+{
+    class ComputerTraceProcess
+    {
+        //Hangi yöntemin kullanılacağı belirleniyor.
+        public static IComputerTrace CreateComputerTrace()
+        {
+            IComputerTrace retVal = new GetLastUserInput.LastUserInput();
+
+            return retVal;
+        }
+    }
+}
